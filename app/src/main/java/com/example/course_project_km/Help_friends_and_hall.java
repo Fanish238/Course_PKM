29fec12b74d965247ed962333905aab7666d42cb
Mikhail <konovalchik1239@mail.ru>
2022-03-18 13:38:38 +0300

    1) Сделал информацию о последних играх. 2) Начал делать Помощь друга. 3) И т.д.

--- app/src/main/java/com/example/course_project_km/Help_friends_and_hall.java
+++ app/src/main/java/com/example/course_project_km/Help_friends_and_hall.java
@@ -3,13 +3,85 @@ package com.example.course_project_km;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Help_friends_and_hall extends AppCompatActivity {
+    TextView OneFriends, TwoFriends, ThreeFriends;
+    int id, id_podraynds;
+    List<String> list = new ArrayList<>();
+    String str_data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help_friends_and_hall);
+        OneFriends = findViewById(R.id.OneFriends);
+        TwoFriends = findViewById(R.id.TwoFriends);
+        ThreeFriends = findViewById(R.id.ThreeFriends);
+        id = getIntent().getIntExtra("id", 0);
+        id_podraynds = getIntent().getIntExtra("id_podraynds", 0);
+        Working_with_files();
+        OneFriends.setText(list.get(0));
+        TwoFriends.setText(list.get(7));
+        ThreeFriends.setText(list.get(14));
     }
+
+    public void Working_with_files() {
+        str_data = gg();
+        InSave();
+        OutSave();
+    }
+
+    public String gg() {
+        String text = "help_friends_rounds_" + id + ".txt";
+        byte[] buffer = null;
+        InputStream is;
+        try {
+            is = getAssets().open(text);
+            int size = is.available();
+            buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return new String(buffer);
+    }
+
+    public void InSave() {
+        try {
+            FileOutputStream FileSave = openFileOutput("help_friends_rounds_" + id + "_COP.txt", MODE_PRIVATE);
+            FileSave.write(str_data.getBytes());
+            FileSave.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void OutSave() {
+        try {
+            FileInputStream FileSave = openFileInput("help_friends_rounds_" + id + "_COP.txt");
+            InputStreamReader reader = new InputStreamReader(FileSave);
+            BufferedReader buffer = new BufferedReader(reader);
+            StringBuilder strBuffer = new StringBuilder();
+            String lines;
+            while ((lines = buffer.readLine()) != null) {
+                strBuffer.append(lines);
+                list.add(lines);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
--- app/src/main/java/com/example/course_project_km/MainActivity.java
+++ app/src/main/java/com/example/course_project_km/MainActivity.java
@@ -9,9 +9,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class MainActivity extends AppCompatActivity {
-    Button button_game, button_info_old_games, button_setting, button_info_games, button_sprayka, button_exit;
+    Button button_game, button_info_old_games, button_setting, button_info_games, button_reference, button_exit;
     AlertDialog.Builder dialog;
+    String SaveUser = "Тут пусто";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
         button_info_old_games = findViewById(R.id.info_old_games);
         button_setting = findViewById(R.id.setting);
         button_info_games = findViewById(R.id.info_games);
-        button_sprayka = findViewById(R.id.sprayka);
+        button_reference = findViewById(R.id.sprayka);
         button_exit = findViewById(R.id.exit);
     }
 
@@ -31,27 +38,63 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void button_info_old_games_onClick(View view) {
-        Toast.makeText(MainActivity.this, R.string.error, Toast.LENGTH_SHORT).show();
+        OutSave();
+        dialog = new AlertDialog.Builder(MainActivity.this);
+        dialog.setIcon(R.drawable.ic_baseline_info_24);
+        dialog.setTitle("Информация о последних играх");
+        dialog.setMessage(SaveUser);
+        dialog.setCancelable(false);
+        dialog.setPositiveButton("Закрыть", (v, dialog) -> v.cancel());
+        dialog.setNegativeButton("Очистка", (v, dialog) -> {
+            try {
+                FileOutputStream save = openFileOutput("save.txt", MODE_PRIVATE);
+                save.write("Тут пусто!".getBytes());
+                save.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        dialog.show();
     }
 
     public void button_setting_onClick(View view) {
         Toast.makeText(MainActivity.this, R.string.error, Toast.LENGTH_SHORT).show();
     }
 
-    public void button_info_games_onClick(View view){
+    public void button_info_games_onClick(View view) {
         dialog = new AlertDialog.Builder(MainActivity.this);
-        dialog.setIcon(R.drawable.lagatip);
+        dialog.setIcon(R.drawable.ic_baseline_info_24);
         dialog.setTitle("Об игре");
+        dialog.setCancelable(false);
+        dialog.setPositiveButton("Закрыть", (v, dialog) -> v.cancel());
         dialog.setMessage(R.string.ob_games);
         dialog.show();
     }
 
-    public void button_sprayka_onClick(View view) {
+    public void button_reference_onClick(View view) {
         dialog = new AlertDialog.Builder(MainActivity.this);
-        dialog.setIcon(R.drawable.lagatip);
+        dialog.setIcon(R.drawable.ic_baseline_info_24);
         dialog.setTitle("Справка");
+        dialog.setCancelable(false);
+        dialog.setPositiveButton("Закрыть", (v, dialog) -> v.cancel());
         dialog.setMessage(R.string.sprayka_text);
         dialog.show();
+    }
+
+    public void OutSave() {
+        SaveUser = "";
+        try {
+            FileInputStream FileSave = openFileInput("save.txt");
+            InputStreamReader reader = new InputStreamReader(FileSave);
+            BufferedReader buffer = new BufferedReader(reader);
+            StringBuilder strBuffer = new StringBuilder();
+            String lines;
+            while ((lines = buffer.readLine()) != null) {
+                SaveUser = strBuffer.append(lines + "\n") + "\n";
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void button_exit_onClick(View view) {
